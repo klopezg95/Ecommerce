d86af93c3a7ba5ff246c7db77a4dde7e39ba2f3e
Anderson Lopez <klopezg95@gmail.com>
2024-07-25 00:14:56 -0400

    Consumo de la Fake API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,34 @@
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
+import { useState, useEffect } from 'react'
+import { data } from 'autoprefixer'
 
 function Home() {
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        //fetch('https://fakestoreapi.com/products')
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(res => res.json())
+        // .then(data => console.log(data))
+        .then(data => setItems(data))
+        
+        
+    }, [])
+    
+
+
     return (
         <Layout>
             Home
-            <Card/>
+            <div className='grid grid-cols-4 gap-10 w-full max-w-screen-lg'>
+            {
+                items?.map((item) =>{
+                    return <Card key={item.id} data={item}/>
+                })
+            }
+            </div>
+
         </Layout>
     )
 }
